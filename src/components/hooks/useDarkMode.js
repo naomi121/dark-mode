54754accc3a18a28c537c99b56e77ff0637e51dc
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2020-02-12 19:28:01 -0500

    added const useDarkMode key initialvalue in line 5

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -1,2 +1,15 @@
 import { useLocalStorage } from "./useLocalStorage"; 
 import { useEffect } from 'react';
+
+
+export const useDarkMode = (key, initialValue) => {
+    const [values, setValues] = useLocalStorage(key, initialValue)
+     useEffect(()=>{
+         if(values === true){
+             (document.querySelector('body').classList.add('dark-mode'))
+         }else{
+             (document.querySelector('body').classList.remove('dark-mode'))
+         }
+     }, [values, setValues])
+     return [values, setValues];
+    };
